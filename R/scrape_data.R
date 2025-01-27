4d696300c7609ba2f20c8338af981bece3a5a7e1
mms1410 <svenmauricemorlock@yahoo.com>
2025-01-27 18:47:28 +0100

    :construction: updated

--- R/scrape_data.R
+++ R/scrape_data.R
@@ -39,11 +39,7 @@ filter_downloads_idx <- function(node) {
     html_elements("a") %>% 
     grep(pattern = "download")
 }
-
-
-
-
-#----------------- Scrape federal coalition contracts --------------------------
+#----------------- Scrape federal coalition contracts spd ----------------------
 
 ## each text document resides within a 'detail section'
 ## each detail section corresponds to a coalition government
@@ -58,13 +54,55 @@ details_spd_contrac <- html_contracts_spd %>%
 idx <- filter_periods_idx(details_spd_contrac, periods_spd)
 details_spd_contrac <- details_spd_contrac[idx]
 
+cat(paste0("Found ", length(details_spd_contrac), " coalition contracts to scrape"))
 
 for (node in details_spd_contrac) {
+  break
   title <- get_title(node)
-  downloads_idx <- filter_downloads_idx(node)
+  
+  # extract election year
+  year_match <- grep(x = title, pattern = "(\\d+)(?=-)",perl = TRUE, value = TRUE)
+  foldername <- regmatches(year_match, regexpr("\\d+(?=-)", match, perl = TRUE))
+  
+  # get all linked pdf files
+  downloads <- node %>% 
+    html_elements("a") %>% 
+    html_attr("href") %>% 
+    grep(pattern = "\\.pdf$", value = TRUE)
+  
+  folderpath <- file.path(path_assets, "pdf","coalition_contracts", "spd", foldername)
+  dir.create(folderpath, recursive = TRUE)
+  
+  cat(paste0("\n---------- Start scraping ", length(downloads)," item(s) for '", title, "' ---------- \n"))
+  
+   for (url_reference in downloads) {
+     
+     cat(paste0("  Scrape url ", url_reference, ":\n"))
+     
+     filename <- sub(".*\\/([^\\/]+\\.pdf)$", "\\1", url_reference)
+     
+     tryCatch({
+         response <- request(url_reference) |> req_perform()
+         
+         }, error = function(e) {
+           cat(paste0("    An unexpected error occured when handling url ", url_reference, "\n"))
+       }
+     )
+     status <- resp_status(response)
+     
+     if (status >= 200 && status <= 299){
+       
+       destination <- file.path(folderpath, filename)
+       
+       writeBin(resp_body_raw(response), destination)
+       
+       if (file.exists(destination))
+         cat(paste0("    Downloaded file into: ", destination, "\n"))
+     }
+   }
 }
-
-#----------------- Scrape federal election programs ----------------------------
+cat("\n--------- Finished scraping SPD contracts ---------\n")
+#----------------- Scrape federal election programs spd -------------------------
 
 ## each text document resides within a detail section
 ## but not all detail sections contain federal party programs
@@ -76,12 +114,68 @@ html_programs_spd <- rvest::read_html(url_programs_spd)
 details_spd_program <- html_programs_spd %>% 
   html_elements("details")
 
-idx <- grep("Bundestagswahl", lapply(details_spd_program, html_text))
+idx <- grep("Bundestagswahl", lapply(X = details_spd_program, FUN = html_text))
 
 details_spd_program <- details_spd_program[idx]
 
+## only for periods where in coalition government
 idx <- strsplit(periods_spd, "-") |>
   sapply(`[`, 1) |>
   filter_periods_idx(x = get_titles(details_spd_program))
 
-get_titles(details_spd_program)[idx]
+
+
+cat(paste0("Found ", length(details_spd_program), " election programmes to scrape"))
+
+
+for (node in details_spd_program[idx]) {
+  break
+  title <- get_title(node)
+  
+  # extract election year
+  foldername <- sub(".*([0-9]{4})$", "\\1", title)
+  
+  downloads <- node %>% 
+    html_elements("a") %>% 
+    html_attr("href")
+  
+  folderpath <- file.path(path_assets, "pdf","election_programmes", "spd", foldername)
+  
+  dir.create(folderpath, recursive = TRUE)
+  
+  cat(paste0("\n---------- Start scraping ", length(downloads)," item(s) for '", title, "' ---------- \n"))
+  
+  for (url_reference in downloads) {
+    
+    cat(paste0("  Scrape url ", url_reference, ":\n"))
+    
+    filename <- sub(".*\\/([^\\/]+\\.pdf)$", "\\1", url_reference)
+    
+    tryCatch({
+      response <- request(url_reference) |> req_perform()
+      
+    }, error = function(e) {
+      cat(paste0("    An unexpected error occured when handling url ", url_reference, "\n"))
+    }
+    )
+    status <- resp_status(response)
+    
+    if (status >= 200 && status <= 299){
+      destination <- file.path(folderpath, filename)
+      writeBin(resp_body_raw(response), destination)
+      if (file.exists(destination))
+        cat(paste0("    Downloaded file into: ", destination, "\n"))
+    }
+  }
+  
+}
+cat("\n--------- Finished scraping SPD election programmes ---------\n")
+
+#----------------- Scrape federal coalition contracts cdu ----------------------
+url_contracts_cdu <-conf[["cdu_contract"]]
+html_contracts_cdu <- rvest::read_html(url_contracts_cdu)
+
+html_contracts_cdu %>% html_elements("a.matomo-download")
+html_contracts_spd %>% html_elements("a") %>% html_attrs()
+
+#----------------- Scrape federal election programs cdu ------------------------
